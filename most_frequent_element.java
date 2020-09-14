e507f90899f9f799a18b2eeb48536a67fe69225c
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2020-09-14 23:13:02 +0530

    Create most_frequent_element.java

---
+++ most_frequent_element.java
@@ -0,0 +1,42 @@
+/*
+	Author : Shaik Faizan Roshan Ali
+	Email : alsahercoder@gmail.com
+	Date : 14-sep-2020
+	
+	This program finds the most frequent occuring element in an array.
+*/
+
+
+import java.util.*;
+
+public class most_freq {
+    public static void main(String[] args) {
+    
+        int[] arr = {1,1,2,2,2,5,5,5,2,3};
+        Arrays.sort(arr);
+        int most_frequent = 0;
+        int counter = 0, max_value = 0;
+        int temp = arr[0];
+
+        // looping through the elements with index values
+        for ( int arr_index = 0; arr_index < arr.length - 1; arr_index++ ) { 
+    
+            if ( temp == arr[arr_index] ) { 
+                counter++;
+
+                if ( counter > max_value ) { 
+                    max_value = counter;
+                    most_frequent = arr_index;
+                }
+            }
+    
+            else { 
+                counter = 1;
+                temp = arr[arr_index + 1]; 
+            }
+        }
+    
+    
+        System.out.println("Most frequent digit : " + arr[most_frequent] + " Number of times : " + max_value);  
+    }   
+}
