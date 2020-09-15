e39bf1c05addd717be93189bddfc29c0c3263f6f
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2020-09-15 12:21:57 +0530

    Create CheckPairs.java

---
+++ CheckPairs.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class CheckPairs {
+	public static void main (String[] args) {
+		
+		 Scanner sumInput = new Scanner(System.in);
+        int[] checkArray = {1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.println("Enter the sum value you want the pair to be : ");
+        int checkSum = sumInput.nextInt();
+        
+        checkPairs(checkArray, checkSum);
+        
+    }
+    
+    public static void checkPairs(int arr[], int sum) {
+        
+        for ( int firstIndex =0; firstIndex < arr.length - 1; firstIndex++ ) {
+            
+            for (int secondIndex = firstIndex + 1; secondIndex < arr.length; secondIndex++) {
+            
+                if ( sum == arr[firstIndex] + arr[secondIndex] ) {
+                
+                        System.out.println("The pair of number that make up the sum " + sum + " are " + arr[firstIndex] + " & "+ arr[secondIndex] );
+                }
+                
+                else
+                    continue;
+            }
+        }
+    }
+}
