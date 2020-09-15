6337219f23ee44eecbe85d1947fc36c93fe51e5f
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2020-09-15 12:26:26 +0530

    Update reverse_array.java

--- reverse_array.java
+++ reverse_array.java
@@ -12,15 +12,13 @@ class reverse_array {
 	public static void main (String[] args) {
 		
 		int[] arr = {10, 20, 30, 40, 50};
-	   int rev_arr[];
-	   rev_arr = new int[arr.length];
+		int rev_arr[];
+	   	rev_arr = new int[arr.length];
 	   
-	   for ( int rev_arr_index = 0; rev_arr_index <= arr.length-1; rev_arr_index++ ) {
+	   	for ( int rev_arr_index = 0; rev_arr_index <= arr.length-1; rev_arr_index++ ) {
 	            
-	        rev_arr[rev_arr_index] = arr[arr.length - 1 - rev_arr_index]; 
-            	           
+	    	rev_arr[rev_arr_index] = arr[arr.length - 1 - rev_arr_index];          
 	   }
-		
 	   System.out.println("Reversed array : " + Arrays.toString(rev_arr));
     }
 }
