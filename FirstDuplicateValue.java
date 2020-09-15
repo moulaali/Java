3053fbd59591da3ba8d340c702921f0dbb349c2c
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2020-09-15 21:13:06 +0530

    Update FirstDuplicateValue.java

--- FirstDuplicateValue.java
+++ FirstDuplicateValue.java
@@ -9,25 +9,27 @@ import java.util.*;
 public class FirstDuplicateValue {
     public static void main(String args[]) {
 	
-		int[] inputArray = {1, 2, 3, 6, 7, 3, 0, 0};
+	int[] inputArray = {1, 2, 3, 6, 7, 3, 0, 0};
 	
-		duplicateValue(inputArray);
+	duplicateValue(inputArray);
+    
+
     }
     
     public static void duplicateValue(int checkArray[]) {
 	
 		for ( int trueIndex = 0; trueIndex < checkArray.length; trueIndex++ ) {
 	    
-	    	int trueValue = checkArray[trueIndex];
+		    int trueValue = checkArray[trueIndex];
 	     
-	    	for ( int checkIndex = trueIndex + 1; checkIndex < checkArray.length; checkIndex++ ) {
-	    
+		    for ( int checkIndex = trueIndex + 1; checkIndex < checkArray.length; checkIndex++ ) {
+		    
 				if ( trueValue == checkArray[checkIndex] ) {
 		 
-		    		System.out.println("The duplicate number: " + trueValue + " with least Index " + trueIndex );
-		    		System.exit(0);
+				    System.out.println("The duplicate number: " + trueValue + " with least Index " + trueIndex );
+				    System.exit(0);
 				}
-			}
+		    }
 		}
     }
 }
