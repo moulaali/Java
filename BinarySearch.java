1942d7e911a789b29f6ba94968a57423e9271e2a
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2020-09-18 16:04:56 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,3 +1,9 @@
+/*
+	Author : Shaik Faizan Roshan Ali
+	Email : alsahercoder@gmail.com
+	Date : 18-sep-2020
+	Binary search algorithm for linearly sorted array.	
+*/
 import java.util.*;
 
 public class BinarySearch {
